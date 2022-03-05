e82cf4bbb99cb6565265bc04e6163a873bca5c61
czarkos <91851570+czarkos@users.noreply.github.com>
2022-03-05 03:04:22 +0200

    TODOs kai sxedon olo to bonus

--- phase1_byhand/byhand.c
+++ phase1_byhand/byhand.c
@@ -1,193 +1,108 @@
-#include<stdio.h>
-#include<string.h>
-#include<assert.h>
-#include<ctype.h>
+#include"byhand.h"
 
-#include"utilities.h"
-
-#define UNKNOWN_TOKEN  -1
-#define END_OF_FILE     0
-#define LE              1
-#define LT              2
-#define NE              3
-#define NOT             4
-#define IDENTIFIER      5
-#define GE              6
-#define GT              7
-#define EQ              8
-#define ASSIGN          9
-#define PLUS            10
-#define MINUS           11
-#define PLUS_PLUS       12
-#define MINUS_MINUS     13
-#define MULT            14
-#define DIV             15
-#define MOD             16
-#define INT             17
-#define DOUBLE          18
-
-/* KEYWORDS */
-#define IF              19
-#define ELSE            20
-#define WHILE           21
-#define FOR             22
-#define FUNCTION        23
-#define RETURN          24
-#define BREAK           25
-#define CONTINUE        26
-#define AND             27
-#define NOTT            28
-#define OR              29
-#define LOCAL           30
-#define TRUE            31
-#define FALSE           32
-#define NIL             33
-
-/* SHMEIA STIKSIS */
-#define LEFT_BRACE          34
-#define RIGHT_BRACE         35
-#define LEFT_BRACKET        36
-#define RIGHT_BRACKET       37
-#define LEFT_PARENTHESIS    38
-#define RIGHT_PARENTHESIS   39
-#define SEMICOLON           40
-#define COMMA               41
-#define COLON               42
-#define DOUBLE_COLON        43
-#define STOP                44
-#define DOUBLE_STOP         45
-
-#define STRING              46
-
-#define LINE_COMMENT        47
-#define BLOCK_COMMENT       48
-#define UNCLOSED_COMMENT    49
-
-#define MAX_STATE 19
-#define TOKEN_SHIFT (MAX_STATE+1)
-#define TOKEN(t)    TOKEN_SHIFT+(t)
-#define STATE(s)    s
-#define istoken(s)  ((s)>MAX_STATE)
-
-int sf0(char c);   int sf1(char c);   int sf2(char c); int sf3(char c);   int sf4(char c);   int sf5(char c);
-int sf6(char c);   int sf7(char c);   int sf8(char c); int sf9(char c);   int sf10(char c);  int sf11(char c);
-int sf12(char c);  int sf13(char c);  int sf14(char c); int sf15(char c); int sf16(char c);  int sf17(char c);
-int sf18(char c);  int sf19(char c);
-
-int (*stateFuncs[MAX_STATE+1])(char) = { &sf0, &sf1, &sf2, &sf3, &sf4, &sf5, &sf6, &sf7, &sf8, &sf9, &sf10, &sf11, &sf12, &sf13, &sf14, &sf15, &sf16, &sf17, &sf18, &sf19};
-
-unsigned gettoken(void);
-
-char names[49][20] = {
-    "LessEqual",
-    "LessThan",
-    "NotEqual",
-    "Not",
-    "Identfier",
-    "GreaterEqual",
-    "GreaterThan",
-    "Equal",
-    "Assignment",
-    "Plus",
-    "Minus",
-    "Plus_Plus",
-    "Minus_minus",
-    "Mult",
-    "Div",
-    "Mod",
-    "Int",
-    "Double",
-    "IF",
-    "ELSE",
-    "WHILE",
-    "FOR",
-    "FUNCTION",
-    "RETURN",
-    "BREAK",
-    "CONTINUE",
-    "AND",
-    "NOT",
-    "OR",
-    "LOCAL",
-    "TRUE",
-    "FALSE",
-    "NIL",
-    "LEFT_BRACE",
-    "RIGHT_BRACE",
-    "LEFT_BRACKET",
-    "RIGHT_BRACKET",
-    "LEFT_PARENTHESIS",
-    "RIGHT_PARENTHESIS",
-    "SEMICOLON",
-    "COMMA",
-    "COLON",
-    "DOUBLE_COLON",
-    "STOP",
-    "DOUBLE_STOP",
-    "STRING",
-    "LINE_COMMENT",
-    "BLOCk_COMMENT",
-    "UNCLOSED_COMMENT"
+struct alpha_token_t
+{
+    unsigned int numline;
+    unsigned int numToken;
+    char *content;
+    char *type;
+    struct alpha_token_t *next;
+    char *specialty;
+    char *category;
 };
 
-int iskeyword(char *s){
-    if(strcmp(s, "if") == 0){
-        return TOKEN(IF);
-    }
-    if(strcmp(s, "else") == 0){
-        return TOKEN(ELSE);
-    }
-    if(strcmp(s, "while") == 0){
-        return TOKEN(WHILE);
-    }
-    if(strcmp(s, "for") == 0){
-        return TOKEN(FOR);
-    }
-    if(strcmp(s, "function") == 0){
-        return TOKEN(FUNCTION);
-    }
-    if(strcmp(s, "return") == 0){
-        return TOKEN(RETURN);
-    }
-    if(strcmp(s, "break") == 0){
-        return TOKEN(BREAK);
-    }
-    if(strcmp(s, "continue") == 0){
-        return TOKEN(CONTINUE);
-    }
-    if(strcmp(s, "and") == 0){
-        return TOKEN(AND);
-    }
-    if(strcmp(s, "not") == 0){
-        return TOKEN(NOTT);
-    }
-    if(strcmp(s, "or") == 0){
-        return TOKEN(OR);
-    }
-    if(strcmp(s, "local") == 0){
-        return TOKEN(LOCAL);
-    }
-    if(strcmp(s, "true") == 0){
-        return TOKEN(TRUE);
-    }
-    if(strcmp(s, "false") == 0){
-        return TOKEN(FALSE);
-    }
-    if(strcmp(s, "nil") == 0){
-        return TOKEN(NIL);
-    }
+typedef struct alpha_token_t alpha_token_t;
 
-    return -24;
+struct token_list
+{
+    alpha_token_t *head;
+    alpha_token_t *tail;
+} tokens;
+
+/* Inserts a token in the struct token list. */
+void insert_token(alpha_token_t *new_token)
+{
+    if (tokens.head == NULL)
+        tokens.head = new_token;
+    else if (tokens.tail == NULL)
+    {
+        tokens.tail = new_token;
+        tokens.head->next = new_token;
+    }
+    else
+    {
+        tokens.tail->next = new_token;
+        tokens.tail = new_token;
+    }
 }
 
-int isPunctuation(char c){
-    if(c=='{'||c=='}'||c=='['||c==']'||c=='('||c==')'||c==';'||c==','||c==':'||c=='.') {
-        //printf("c=%c", c);
-        return 1;
-    }
+/* Prints the struct token list. */
+void print_all()
+{
+    alpha_token_t *iter = tokens.head;
+    printf("------------------- Lexical Analysis ----------------------\n");
+    //printf("line\tnumofToken\tContent\tType\tSpecialty\tCategory\n");
+    while (iter)
+    {
+        if (strcmp(iter->type, "STRING") == 0)
+        {
+            printf("%u:\t#%u\t\"%s\"\t%s\t\"%s\"\t<--%s\n", iter->numline, iter->numToken, iter->content, iter->type, iter->specialty, iter->category);
+        }
+        else if (strcmp(iter->type, "COMMENT") == 0)
+        {
 
-    return 0;
+            printf("%u:\t#%u\t\"%s\"\t%s\t%s\t<--%s\n", iter->numline, iter->numToken, iter->content, iter->type, iter->specialty, iter->category);
+        }
+        else
+        {
+            printf("%u:\t#%u\t\"%s\"\t%s\t%s\t<--%s\n", iter->numline, iter->numToken, iter->content, iter->type, iter->specialty, iter->category);
+        }
+
+        iter = iter->next;
+    }
 }
+
+/* Frees the struct token list. */
+void free_all()
+{
+    alpha_token_t *iter = tokens.head, *tmp;
+    while (iter)
+    {
+        tmp = iter;
+        iter = iter->next;
+        free(tmp->content);
+        free(tmp);
+    }
+}
+
+/* Inserts the data of a token in its struct node alpha_token_t. */
+void insert_data(int numline, int numToken, char *content, char *type, alpha_token_t* yylval, char* specialty)
+{
+    yylval->numline = numline;
+    yylval->numToken = numToken;
+    yylval->content = (char *) malloc(strlen(content) * sizeof(char));
+    strcpy(yylval->content, content);
+    yylval->type = type;
+    yylval->next = NULL;
+
+    //if specialty is null it means copy the content to the specialty else assign the new one
+    if(specialty==NULL){
+      yylval->specialty = yylval->content;
+    }else{
+      yylval->specialty = specialty;
+    }    
+    if((strcmp(type,"STRING") == 0) || (strcmp(type,"IDENTIFIER") == 0)){
+      yylval->category = "char*";
+    }else if(strcmp(type,"CONST_INT") == 0){
+      yylval->category = "integer";
+    }else if(strcmp(type,"CONST_DOUBLE") == 0){
+      yylval->category = "double";
+    }else{
+      yylval->category = "enumerated";
+    }  
+}
+
+int num_tokens=0;
 
 int main(int argc, char *argv[]) {
     if(argc > 1)
@@ -197,14 +112,37 @@ int main(int argc, char *argv[]) {
     unsigned return_token;
     while((return_token = gettoken()) != END_OF_FILE) {
         if(return_token == UNKNOWN_TOKEN){
-            printf("UNKNOWN_TOKEN: \'%s\'\n", GetLexeme());
+            printf("UNKNOWN_TOKEN: \'%s\' in line %d\n", GetLexeme(), lineNo);
         }
-        else if((isk = iskeyword(lexeme)) > 0)
-            printf("%s which is a KEYWORD in line %d\n", names[isk-TOKEN_SHIFT-1], lineNo);
-        else
-            printf("%s in line %d\n", names[return_token-1], lineNo);
+        else {
+            alpha_token_t* curr = malloc(sizeof(alpha_token_t));
+            
+            if(return_token == IDENTIFIER){
+                if((isk = iskeyword(GetLexeme())) > 0)
+                    insert_data(lineNo, ++num_tokens, lexeme, "KEYWORD", curr, names[isk-TOKEN_SHIFT-1]);
+                else
+                insert_data(lineNo, ++num_tokens, lexeme, "IDENTIFIER", curr, NULL);
+                //printf("%s in line %d\n", names[return_token-1], lineNo);
+            }
+            else if(return_token == INT)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "CONST_INT", curr, NULL);
+            else if(return_token == DOUBLE)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "CONST_DOUBLE", curr, NULL);
+            else if(return_token == STRING)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "STRING", curr, NULL);
+            else if(return_token > 0 && return_token < 17)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "OPERATOR", curr, names[return_token-1]);
+            else if(return_token > 33 && return_token < 46)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "PUNCTUATION", curr, names[return_token-1]);
+            else if(return_token > 46 && return_token < 50)
+                insert_data(lineNo, ++num_tokens, GetLexeme(), "COMMENT", curr, names[return_token-1]);
+            
+            insert_token(curr);
+        }
     }
 
+    print_all();
+    free_all();
     return 0;
 }
 
@@ -227,7 +165,7 @@ unsigned gettoken() {
             return UNKNOWN_TOKEN;
         }
         else if(istoken(state)) {
-            printf("Found token \'%s\' with type ", GetLexeme());
+            //printf("Found token \'%s\' with type ", GetLexeme());
             return state - TOKEN_SHIFT;
         }
         else if(!isspace(c) && !useLookAhead && c != '\"')
@@ -521,8 +459,12 @@ int sf19(char c){
             CheckLine(c);
             if(c == '/'){
                 total_comments--;
-                if(total_comments != 0)
+                if(total_comments != 0){
                     printf("Found token of type NESTED_COMMENT in line %d\n", lineNo);
+                    alpha_token_t* curr = malloc(sizeof(alpha_token_t));
+                    insert_data(lineNo, ++num_tokens, GetLexeme(), "COMMENT", curr, "NESTED_COMMENT");
+                    insert_token(curr);
+                }
             }
         }
         c = GetNextChar();
