342f73cd9c5f16100f66bdcce500a8ec4fc9cc3e
Dodos626 <chlktheo@gmail.com>
2022-03-25 12:07:21 +0200

    Create defines.h

---
+++ phase_2/defines.h
@@ -0,0 +1,64 @@
+#define UNKNOWN_TOKEN  -1
+#define END_OF_FILE     0
+/* OPERATORS */
+#define LE              1
+#define LT              2
+#define NE              3
+#define IDENTIFIER      4
+#define GE              5
+#define GT              6
+#define EQ              7
+#define ASSIGN          8
+#define PLUS            9
+#define MINUS           10
+#define PLUS_PLUS       11
+#define MINUS_MINUS     12
+#define MULT            13
+#define DIV             14
+#define MOD             15
+/* INTEGER */
+#define INT             16
+/* DOUBLE*/
+#define DOUBLE          17
+
+/* KEYWORDS */
+#define IF              18
+#define ELSE            19
+#define WHILE           20
+#define FOR             21
+#define FUNCTION        22
+#define RETURN          23
+#define BREAK           24
+#define CONTINUE        25
+#define AND             26
+#define NOTT            27
+#define OR              28
+#define LOCAL           29
+#define TRUE            30
+#define FALSE           31
+#define NIL             32
+
+/* PUNCTUATION */
+#define LEFT_BRACE          33
+#define RIGHT_BRACE         34
+#define LEFT_BRACKET        35
+#define RIGHT_BRACKET       36
+#define LEFT_PARENTHESIS    37
+#define RIGHT_PARENTHESIS   38
+#define SEMICOLON           39
+#define COMMA               40
+#define COLON               41
+#define DOUBLE_COLON        42
+#define STOP                43
+#define DOUBLE_STOP         44
+
+/* STRING */
+#define STRING              45
+
+/* COMMENT */
+#define LINE_COMMENT        46
+#define BLOCK_COMMENT       47
+
+/* UNCLOSED */
+#define UNCLOSED_COMMENT    48
+#define UNCLOSED_STRING     49
